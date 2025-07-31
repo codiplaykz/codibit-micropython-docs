bfaa54fefe60ca34853fa90053e0a87bf956a83a
Sarang <srngch.dev@gmail.com>
2025-07-31 12:19:08 +0900

    Set up deployment with Github Actions

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://codiplaykz.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/codibit-micropython-docs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'codiplaykz', // Usually your GitHub org/user name.
-  projectName: 'codibit-micropython', // Usually your repo name.
+  projectName: 'codibit-micropython-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
